d7627f19cf729f6fadf9552b19e94cb82a58f325
Joshua Pemberton <joshp2865@gmail.com>
2020-03-07 15:31:23 +0000

    Create forge.config.js

---
+++ forge.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    packagerConfig: {},
+    makers: [
+        {
+          name: "@electron-forge/maker-squirrel",
+          config: {
+            name: "Livida",
+            setupExe: "Livida Setup.exe",
+            exe: "Livida.exe",
+            iconUrl: "https://livida.net/assets/favicon.ico",
+            setupIcon: "assets/favicon.ico"
+          }
+        },
+        {
+          name: "@electron-forge/maker-zip",
+          platforms: [
+            "darwin"
+          ]
+        },
+        {
+          name: "@electron-forge/maker-deb",
+          config: {}
+        },
+        {
+          name: "@electron-forge/maker-rpm",
+          config: {}
+        }
+      ],
+    }
