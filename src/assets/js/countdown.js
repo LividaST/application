b5f2eb2523bb053f53f72914527a12d56c0bad9b
Joshua Pemberton <joshp2865@gmail.com>
2020-08-04 18:30:49 +0100

    RiseFM App
    lol why not?

---
+++ src/assets/js/countdown.js
@@ -0,0 +1,105 @@
+const checkVolume = () => {
+    $('.volume-slider').val($('.volume-slider').val());
+    $('#stream')[0].volume = $('.volume-slider').val();
+};
+
+$('.volume-slider')
+.on('click', checkVolume)
+.on('mousemove', checkVolume)
+.on('mousedown', checkVolume)
+.on('mouseup', checkVolume);
+window.onload = () => { 
+    $.getJSON("../package.json", data => {
+        $("#ver").html(`v${data.version}`);
+    })
+    let count = 0;
+    setInterval(() => {
+        const cd = $("#cd");
+        const delay = $("#del");
+        const stream = $('#stream')[0];
+        if(stream.paused) {
+            count++
+            delay.html(count);
+            cd.fadeIn("slow");
+        }
+    }, 1000)
+}
+
+const togglePlay = () => {
+    const stream = $('#stream');
+    const button = $('.play-button');
+    if (stream[0].paused) {
+        button.removeClass('fa-play');
+        button.removeClass('fa-pause');
+        button.addClass('fa-spinner-third');
+        stream.attr('src', 'https://stream.livida.net/');
+        checkVolume();
+        stream[0].play()
+        .then(() => {
+            button.removeClass('fa-play');
+            button.removeClass('fa-spinner-third');
+            button.addClass('fa-pause');
+        })
+        .catch(() => {
+            button.removeClass('fa-pause');
+            button.removeClass('fa-spinner-third');
+            button.addClass('fa-play');
+        });
+    } else {
+        stream[0].pause();
+        button.removeClass('fa-pause');
+        button.removeClass('fa-spinner-third');
+        button.addClass('fa-play');
+    };
+};
+
+
+const updateStats = () => {
+    $.get('https://api.livida.net/api/radio/livida', (res) => {
+        const { data } = res;
+        fetchJsonp('https://api.deezer.com/search/track/autocomplete?limit=1&q=' + data.song.name + ` ${data.song.artist}` + '&output=jsonp')
+        .then(res => res.json())
+        .then(res => {
+            if (res.data[0]) {
+                $('.song-art').attr('src', res.data[0].album.cover || `./assets/img/Livida.png`);
+                $('.artist-image').attr('src', res.data[0].artist.picture || `./assets/img/Livida.png`);
+            } else {
+                $('.song-art').attr('src', `./assets/img/Livida.png`);
+                $('.artist-image').attr('src', `./assets/img/Livida.png`);
+            };
+        })
+        .catch((err) => {
+            console.error(`Error whilst fetching from Deezer for ${playing.song}:`, err);
+            $('.song-art').attr('src', `./assets/img/Livida.png`);
+            $('.artist-image').attr('src', `./assets/img/Livida.png`);
+        });
+        $('.song-title').text(data.song.name);
+        $('.song-artist').text(data.song.artist);
+        $('.dj-name').text(data.dj);
+        const songText = `${data.song.name} by ${data.song.artist}`;
+        if (window.prevSongText != songText) {
+            $('.song-text').text(songText);
+            if ($('.song-text').parent().prop('scrollHeight') > $('.song-text').parent().height() + 16) {
+                const t = $('<div></div>').html(songText).text(),
+                      m = `<marquee direction="right" scrollamount="8">${t}</marquee>`;
+                $('.song-text').html(m);
+            };
+        };
+        window.prevSongText = songText;
+    })
+    .fail((err) => {
+        console.error(`Error whilst fetching song metadata:`, err);
+        $('.song-art').attr('src', `./assets/img/Livida.png`);
+        $('.artist-image').attr('src', `./assets/img/Livida.png`);
+        $('.song-title').text('Stream Offline');
+        $('.song-artist').text('Unknown');
+        $('.dj-name').text('Stream Offline');
+        $('.listeners').text('Unknown');
+    });
+};
+
+updateStats();
+setInterval(updateStats, 5000);
+togglePlay();
+
+$('.play-button').click(togglePlay);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const { app, BrowserWindow, Menu, autoUpdater, dialog } = require('electron');
+const { app, BrowserWindow, Menu, autoUpdater, dialog } = require('electron')
 const shell = require('electron').shell
-const fetch = require('node-fetch');
-const client = require('discord-rich-presence')('602573554780733450');
+const fetch = require('node-fetch')
+const client = require('discord-rich-presence')('602573554780733450')
 const server = 'https://dl.livida.net'
 const feed = `${server}/update/${process.platform}/${app.getVersion()}`
 
@@ -32,92 +32,77 @@ autoUpdater.on('error', message => {
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+  app.quit()
 }
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow
 
 const createWindow = () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 500,
-    height: 200,
-  });
+    width: 750,
+    height: 400,
+    frame: false,
+    transparent: true
+  })
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   // Emitted when the window is closed.
   mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-};
+    mainWindow = null
+  })
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 
- app.on('ready', function(){
-   createWindow()
-   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-   Menu.setApplicationMenu(mainMenu);
- })
- 
- const mainMenuTemplate = [
-   {
-     label: 'Livida',
-     submenu: [
-       {label:'Discord',
-        click() {
-          shell.openExternal('https://discord.gg/6vRE5NJ')
-        }}
-     ]
-   }
- ];
+app.on('ready', function () {
+  createWindow()
+})
+
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
 
 const updateSong = async () => {
   try {
-    let data = await (await fetch(`https://radio.livida.net/api/nowplaying/1`)).json();
-    let { now_playing: np } = data;
-    let { song } = np;
-    let { artist, title} = song;
+    const { data } = await (await fetch('https://api.livida.net/api/radio/livida')).json()
     client.updatePresence({
-      details: `Listening to Livida Radio`,
-      state: `${title} by ${artist}`,
+      details: `🎵 | ${data.song.name} by ${data.song.artist}`,
+      state: `🎙️ | ${data.dj}`,
       largeImageKey: 'icon',
       largeImageText: 'livida.net',
       smallImageKey: 'crown',
-      instance: true,
-    });
+      instance: true
+    })
   } catch (err) {
-    console.error(err);
+    console.error(err)
   };
-};
+}
 
-updateSong();
-setInterval(updateSong, 5000);
-
+updateSong()
+setInterval(updateSong, 5000)
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
